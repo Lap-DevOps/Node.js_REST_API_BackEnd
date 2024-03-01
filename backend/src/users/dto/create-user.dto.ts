85af084891223c7ccd95c76c2b94d1095e2f44d5
Andrii L <99610122+Lap-DevOps@users.noreply.github.com>
2024-03-01 14:46:23 +0000

    feat: add swagger

--- backend/src/users/dto/create-user.dto.ts
+++ backend/src/users/dto/create-user.dto.ts
@@ -1,4 +1,9 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class CreateUserDto {
+    @ApiProperty({example: 'qEJ6s@example.com', description: 'User email'})
     readonly email: string;
+
+    @ApiProperty({example: '12345', description: 'User password'})
     readonly password: string;
 }
--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,20 +1,27 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
+import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { User } from './user.model';
 
+
+@ApiTags('Users')
 @Controller('users')
 export class UsersController {
 
     constructor(private usersService: UsersService) { }
 
 
-
+    @ApiOperation({ summary: 'Create user' })
+    @ApiOkResponse({ status: 201, type: User })
     @Post()
     async create(@Body() userdto: CreateUserDto) {
 
         return this.usersService.createUser(userdto);
     }
 
+    @ApiOperation({ summary: 'Get all users' })
+    @ApiOkResponse({ status: 200, type: [User] })
     @Get()
     async getAll() {
         return this.usersService.getAllUsers();
