153919d34a8cd3e1a467790ce1b6eee308d16875
Andrii L <99610122+Lap-DevOps@users.noreply.github.com>
2024-03-01 14:09:57 +0000

    feat: User Model + +

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,6 +6,7 @@ import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
+import { User } from './users/user.model';
 
 @Module({
   controllers: [],
@@ -25,7 +26,7 @@ import { ConfigModule } from '@nestjs/config';
       username: process.env.POSTGRES_USER,
       password: String(process.env.POSTGRES_PASSWORD),
       database: process.env.POSTGRES_DB,
-      models: [],
+      models: [User],
       autoLoadModels: true,
 
     }),
